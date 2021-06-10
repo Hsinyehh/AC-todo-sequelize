68575bdf8403d052c5c8d95a4d1bb96225ac94de
Rita <hsen.yeh123@gmail.com>
2021-06-10 15:42:09 +0800

    feat: add logout &　refactor: routes

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+const home = require('./modules/home')
+const todos = require('./modules/todos')
+const users = require('./modules/users')
+//const auth = require('./modules/auth')
+
+
+//const { authenticator } = require('../middleware/auth')  // 掛載 middleware
+
+router.use('/todos', todos)
+
+router.use('/users', users)
+
+//router.use('/auth', auth)
+
+
+router.use('/', home)
+
+
+// 準備引入路由模組
+// 匯出路由器
+module.exports = router
