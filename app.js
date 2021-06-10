3d8d1b98c9fd1fd4810290b1f383a9ee4450ae23
Rita <hsen.yeh123@gmail.com>
2021-06-10 16:24:00 +0800

    feat: add facebook login

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const usePassport = require('./config/passport')
 const passport = require('passport')
+const flash = require('connect-flash')
 const routes = require('./routes')
 
 
@@ -25,6 +26,14 @@ app.use(methodOverride('_method'))
 
 usePassport(app)
 
+app.use(flash())  // 掛載套件
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
 
 
 app.use(routes)
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
+const FacebookStrategy = require('passport-facebook').Strategy
 const db = require('../models')
 const User = db.User
 
@@ -32,4 +33,31 @@ module.exports = app => {
         done(null, user)
       }).catch(err => done(err, null))
   })
+
+
+  passport.use(new FacebookStrategy({
+    clientID: '2945896305647768',
+    clientSecret: 'ff315aa078aff9e029c9f6eea362584f',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['email', 'displayName']
+  },
+    (accessToken, refreshToken, profile, done) => {
+      const { name, email } = profile._json
+      User.findOne({ name })
+        .then(user => {
+          if (user) return done(null, user)
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => User.create({
+              name,
+              email,
+              password: hash
+            }))
+            .then(user => done(null, user))
+            .catch(err => done(err, null))
+        })
+    }
+  ))
 }
